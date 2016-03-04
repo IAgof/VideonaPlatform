d274b30083e3b4814eeb1e86592e26dd312f9fcc
Javier Liarte <jliarte@gmail.com>
2016-03-04 17:03:01 +0100

    Created test set up for using db and alembic migrations
    - Updated alembic.ini with script_location for testing to work
    - Added db and session fixtures to conftest.py
    - Added test_settings file with specific settings for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,68 @@
     Defines our app fixture
 """
 import pytest
+from alembic.command import upgrade
+from alembic.config import Config
 from videona_platform.factory import create_app
+from videona_platform.core import db as _db
+import test_settings
 
 
-@pytest.fixture
+ALEMBIC_CONFIG = 'migrations/alembic.ini'
+
+
+@pytest.fixture(scope='session')
 def app():
-    app = create_app()
+    app = create_app(test_settings)
     return app
+
+
+def apply_migrations():
+    """ Applies all alembic migrations. """
+    config = Config(ALEMBIC_CONFIG)
+    upgrade(config, 'head')
+
+
+@pytest.fixture(scope='session')
+def db(app, request):
+    """
+    Create session-wide test database
+    :param app: app fixture
+    :param request:
+    :return:
+    """
+    def teardown():
+        _db.drop_all()
+
+    _db.app = app
+    # Apply already created migrations
+    apply_migrations()
+    # And create pending models, not already as migrations, for testing purposes
+    _db.create_all()
+
+    request.addfinalizer(teardown)
+    return _db
+
+
+@pytest.fixture(scope='function')
+def session(db, request, monkeypatch):
+    """
+    Creates a new database session for a test
+    :param db: db fixture
+    :param request:
+    :return:
+    """
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    # Patch Flask-SQLAlchemy to use our connection instead of replacing the whole session
+    # See https://github.com/mitsuhiko/flask-sqlalchemy/pull/249
+    monkeypatch.setattr(_db, 'get_engine', lambda *args: connection)
+
+    def teardown():
+        _db.session.remove()
+        transaction.rollback()
+        connection.close()
+
+    request.addfinalizer(teardown)
+    return _db.session
