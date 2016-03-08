5a9e2889d60c57ff6168d2eff21f1d8e6f5087b4
Javier Liarte <jliarte@gmail.com>
2016-03-08 22:26:21 +0100

    Updated frontend module to use templates
    - Adding template_folder to blueprint definition
    - Using render_template in hello_world view
    - Created tests for view and template using (learning how to test some things in flask ;))
    - Created context manager patch_view_function to help patching for testing views

--- tests/frontend/frontend_landing_test.py
+++ tests/frontend/frontend_landing_test.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 import pytest
+import mock
 from hamcrest import *
+from contextlib import contextmanager
 from flask import Blueprint, url_for
 from videona_platform.frontend.frontend import front_page_blueprint
+
+@contextmanager
+def patch_view_function(app, view):
+        original_function = app.view_functions[view]
+        app.view_functions[view] = patched_function = mock.Mock()
+        patched_function.return_value = 'view patched!'
+
+        try:
+            yield patched_function
+        finally:
+            """Unpatch the view."""
+            app.view_functions[view] = original_function
 
 
 class TestLandingRoutes(object):
@@ -13,7 +27,26 @@ class TestLandingRoutes(object):
         assert_that(front_page_blueprint.url_prefix, none())
         assert_that(front_page_blueprint.name, is_in(app.blueprints))
 
+
     def test_bp_main_route(self, client):
         response = client.get(url_for('front_page.hello_world'))
 
         assert_that(response.status_code, is_(200))
+
+
+    def test_root_calls_hello_world_view(self, app, client):
+        with patch_view_function(app, 'front_page.hello_world') as hello_world:
+
+            client.get('/')
+
+            assert_that(hello_world.called, is_(True))
+
+
+    @mock.patch('videona_platform.frontend.frontend.render_template', autospec=True)
+    def test_template_used(self, render_template, client):
+        render_template.return_value = 'rendered'
+
+        client.get('/')
+
+        assert_that(render_template.called, is_(True))
+        render_template.assert_called_once_with('front_page.html')
--- videona_platform/frontend/frontend.py
+++ videona_platform/frontend/frontend.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-front_page_blueprint = Blueprint('front_page', __name__)
+
+front_page_blueprint = Blueprint('front_page', __name__, template_folder='templates',)
 
 
 @front_page_blueprint.route('/')
 def hello_world():
-    return '#VideonaTime!'
+    return render_template('front_page.html')
