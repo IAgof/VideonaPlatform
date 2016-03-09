7c31172960d10658de597e264e7d9d99ccb9ebc2
Javier Liarte <jliarte@gmail.com>
2016-03-09 09:54:46 +0100

    Modified frontend module to use styles. Module static dirs configured in Blueprint
    - Created static folders structure
    - Created compass/sass project in config.rb
    - Added package.json and react vendor lib

--- videona_platform/frontend/frontend.py
+++ videona_platform/frontend/frontend.py
@@ -2,7 +2,8 @@
 from flask import Blueprint, render_template
 
 
-front_page_blueprint = Blueprint('front_page', __name__, template_folder='templates',)
+front_page_blueprint = Blueprint('front_page', __name__, template_folder='templates',
+                                 static_folder='static', static_url_path='/frontend/static')
 
 
 @front_page_blueprint.route('/')
