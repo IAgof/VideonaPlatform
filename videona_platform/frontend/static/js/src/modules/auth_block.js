07eaebf474cb368e3fce195972cb4d45fd577d1f
Javier Liarte <jliarte@gmail.com>
2016-04-27 16:22:26 +0200

    Refactoring auth block react component to ES6

--- videona_platform/frontend/static/js/src/modules/auth_block.js
+++ videona_platform/frontend/static/js/src/modules/auth_block.js
@@ -6,30 +6,31 @@ import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import VideonaRawTheme from '../material_theme';
 
 
-var SignupButton = React.createClass({
-    //the key passed through context must be called "muiTheme"
-    childContextTypes: {
-        muiTheme: React.PropTypes.object,
-    },
-
-    getChildContext: function() {
+class SignupButton extends React.Component {
+    getChildContext() {
         return {
-            muiTheme: ThemeManager.getMuiTheme(VideonaRawTheme),
+            muiTheme: ThemeManager.getMuiTheme(VideonaRawTheme)
         };
-    },
+    }
 
-    render: function()  {
+    render()  {
         return (
             <RaisedButton label="Signup" href="/" hoverColor="#ddd" className="signup-button" primary="true" />
         );
     }
-});
+};
 
-var LoginButton = React.createClass({
-    render: function() {
-    return (<FlatButton label="Login" href="/login" className="login-button" hoverColor="#ddd" secondary="true" />)
+//the key passed through context must be called "muiTheme"
+SignupButton.childContextTypes = {
+    muiTheme: React.PropTypes.object
+};
+
+
+class LoginButton extends React.Component {
+    render() {
+        return (<FlatButton label="Login" href="/login" className="login-button" hoverColor="#ddd" secondary="true" />)
     }
-    })
+}
 
 
 
