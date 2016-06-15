dc89755e5cd4b14ea7802b67627a7eee7e20f5dc
Javier Liarte <jliarte@gmail.com>
2016-06-15 16:58:41 +0200

    Created new LoginDialog react component to handle user auth
    - New component LoginDialog in login_card.js
    - Rendering LoginDialog in main page auth_block
    - New vertical and horizontal center sass mixins
    - Added csrf token to meta tag in front page template

--- videona_platform/frontend/static/js/src/modules/auth_block.js
+++ videona_platform/frontend/static/js/src/modules/auth_block.js
@@ -6,30 +6,45 @@ import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import VideonaRawTheme from '../material_theme';
 
 
-var SignupButton = React.createClass({
-    //the key passed through context must be called "muiTheme"
-    childContextTypes: {
-        muiTheme: React.PropTypes.object,
-    },
+class SignupButton extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleOpen = this.handleOpen.bind(this)
+    }
 
-    getChildContext: function() {
+    handleOpen() {
+        console.log('touched')
+        alert('cant touch this!')
+    }
+
+    getChildContext() {
         return {
-            muiTheme: ThemeManager.getMuiTheme(VideonaRawTheme),
+            muiTheme: ThemeManager.getMuiTheme(VideonaRawTheme)
         };
-    },
+    }
 
-    render: function()  {
+    handleTouch() {
+    }
+
+    render()  {
         return (
-            <RaisedButton label="Signup" href="/" hoverColor="#ddd" className="signup-button" primary="true" />
+            <RaisedButton label="Signup" href="/" hoverColor="#ddd" className="signup-button"
+            primary={true} onTouchTap={this.handleOpen} />
         );
     }
-});
+};
 
-var LoginButton = React.createClass({
-    render: function() {
-    return (<FlatButton label="Login" href="/login" className="login-button" hoverColor="#ddd" secondary="true" />)
+//the key passed through context must be called "muiTheme"
+SignupButton.childContextTypes = {
+    muiTheme: React.PropTypes.object
+};
+
+
+class LoginButton extends React.Component {
+    render() {
+        return (<FlatButton label="Login" href="/login" className="login-button" hoverColor="#ddd" secondary={true} />)
     }
-    })
+}
 
 
 
