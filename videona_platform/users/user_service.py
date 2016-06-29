ccc2f5d347c35582705708ef31f442e75a975015
Javier Liarte <jliarte@gmail.com>
2016-06-29 19:38:28 +0200

    Added password length validator to register users API endpoint

--- videona_platform/users/user_service.py
+++ videona_platform/users/user_service.py
@@ -7,7 +7,9 @@
 """
 from flask_security.utils import encrypt_password
 
+from videona_platform.default_settings import MINIMUN_PASSWORD_LENGTH
 from videona_platform.factory import user_datastore
+from videona_platform.users import models as users_models
 from videona_platform.core import db, VideonaError
 
 
@@ -16,6 +18,7 @@ class UserService(object):
     ERROR_USER_ALREADY_EXISTS = 'User already exists'
 
     def register(self, email, password):
+        self._check_password_lenght(password)
         password = encrypt_password(password)
         if user_datastore.find_user(email=email):
             raise VideonaError(UserService.ERROR_USER_ALREADY_EXISTS)
@@ -29,5 +32,10 @@ class UserService(object):
             db.session.rollback()
             raise VideonaError(UserService.ERROR_DATABASE_ERROR)
             # db.session.flush()
+
+    def _check_password_lenght(self, password):
+        if len(password) < MINIMUN_PASSWORD_LENGTH:
+            raise VideonaError(users_models.User.USER_ERROR_PASSWORD_TOO_SHORT)
+
 
 users = UserService()
